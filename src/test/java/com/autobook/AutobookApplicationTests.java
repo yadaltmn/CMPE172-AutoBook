618b757a4e73c881e9dbaf9a5b2acd9d8e8aad17
yadaltmn <jada-lien.nguyen@sjsu.edu>
2026-09-24 20:46:52 -0700

    Configure AutoBook Spring Boot dependencies

---
+++ src/test/java/com/autobook/AutobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
